605e13ffe772a2f6f1d9bb1bfbcb68f9992d5c42
Michael Farrell <micolous+git@gmail.com>
2026-08-10 16:55:28 +1000

    WIP: Bug 2057377 - caBLE QR-initiated transaction crypto
    * Generate and parse the `FIDO:/` URLs used by the initiator to share key
      material and request parameters to the authenticator.
    
    * Generate, parse, encrypt and decrypt the BLE advertisement payloads used
      by the authenticator to reply to the initiator.
    
    There are a bunch of TODOs in this change for API improvements and bug
    work-arounds in NSS and `nss-rs`.
    
    Differential Revision: https://phabricator.services.mozilla.com/D313918

--- security/manager/ssl/noise/src/ec.rs
+++ security/manager/ssl/noise/src/ec.rs
@@ -6,8 +6,11 @@
 
 use crate::{Error, Result};
 #[cfg(feature = "xpcom")]
-use nss_rs::ec::{convert_to_public, EcdhKeypair, EcdhPrivateKey};
-use nss_rs::{der, ec::EcdhPublicKey};
+use nss_rs::ec::{convert_to_public, EcdhPrivateKey};
+use nss_rs::{
+    der,
+    ec::{import_ec_public_key_from_spki, EcdhKeypair, EcdhPublicKey},
+};
 
 pub const P256_X962_LENGTH: usize = 65;
 const P256_X962_DER_LENGTH: usize = SECP256R1_DER_PUBKEY_HEADER.len() + P256_X962_LENGTH;
@@ -55,6 +58,46 @@ const SECP256R1_DER_ALT_BYTES: [u8; 3] = [4, P256_X962_LENGTH as u8, 4];
 const P256_X962_BITSTRING_LENGTH: usize = P256_X962_LENGTH + 2;
 
 pub const P256_X962_COMPRESSED_LENGTH: usize = 33;
+const P256_X962_COMPRESSED_DER_LENGTH: usize =
+    SECP256R1_COMPRESSED_DER_PUBKEY_HEADER.len() + P256_X962_COMPRESSED_LENGTH;
+
+/// Static DER header for an uncompressed SEC.1 P-256 point in DER format.
+// TODO: rewrite with `concat_bytes!` once stable
+const SECP256R1_COMPRESSED_DER_PUBKEY_HEADER: [u8; 26] = [
+    // SubjectPublicKeyInfo
+    der::TAG_SEQUENCE,
+    (24 + P256_X962_COMPRESSED_LENGTH) as u8,
+    // algorithm: AlgorithmIdentifier
+    der::TAG_SEQUENCE,
+    0x13,
+    // algorithm
+    der::TAG_OBJECT_ID,
+    0x07,
+    // OID_EC_PUBLIC_KEY_BYTES
+    0x2a,
+    0x86,
+    0x48,
+    0xce,
+    0x3d,
+    0x02,
+    0x01,
+    // parameters
+    der::TAG_OBJECT_ID,
+    0x08,
+    // OID_SECP256R1_BYTES
+    0x2a,
+    0x86,
+    0x48,
+    0xce,
+    0x3d,
+    0x03,
+    0x01,
+    0x07,
+    // subjectPublicKey
+    der::TAG_BIT_STRING,
+    (P256_X962_COMPRESSED_LENGTH + 1) as u8,
+    0x00,
+];
 
 #[cfg(feature = "xpcom")]
 /// Convert an [`EcdhPrivateKey`] into an [`EcdhKeypair`].
@@ -75,6 +118,45 @@ pub fn sec1_ec2_key_to_der(key: &[u8; P256_X962_LENGTH]) -> Result<Vec<u8>> {
     o.extend_from_slice(key);
 
     Ok(o)
+}
+
+/// Convert a compressed SEC.1 P-256 point into DER format for NSS.
+pub fn compressed_sec1_ec2_key_to_der(key: &[u8; P256_X962_COMPRESSED_LENGTH]) -> Result<Vec<u8>> {
+    if key[0] != 0x02 && key[0] != 0x03 {
+        // incorrect format
+        return Err(Error::InvalidArgument);
+    }
+
+    let mut o = Vec::with_capacity(P256_X962_COMPRESSED_DER_LENGTH);
+    o.extend_from_slice(&SECP256R1_COMPRESSED_DER_PUBKEY_HEADER);
+    o.extend_from_slice(key);
+
+    Ok(o)
+}
+
+/// Convert an [`EcdhPublicKey`] to compressed SEC.1 bytes.
+///
+/// This returns [`P256_X962_COMPRESSED_LENGTH`] bytes.
+pub fn ec2_pubkey_to_compressed_sec1(key: &EcdhPublicKey) -> Result<Vec<u8>> {
+    // TODO: This should be a proper API: https://github.com/mozilla/nss-rs/issues/140
+    // TODO: Check key type, needs https://github.com/mozilla/nss-rs/pull/121
+    let mut pub_bytes = key.key_data()?;
+    if pub_bytes.len() == P256_X962_COMPRESSED_LENGTH && (pub_bytes[0] == 2 || pub_bytes[0] == 3) {
+        // PK11_HPKE_Serialize always outputs in the form it was imported in, so if that was
+        // compressed, it stays that way: https://github.com/mozilla/nss-rs/issues/136.
+        //
+        return Ok(pub_bytes);
+    }
+
+    if pub_bytes.len() != P256_X962_LENGTH || pub_bytes[0] != 4 {
+        // incorrect format
+        return Err(Error::InvalidArgument);
+    }
+
+    // 0x02 = even, 0x03 = odd
+    pub_bytes[0] = (pub_bytes[64] & 1) | 2;
+    pub_bytes.truncate(P256_X962_COMPRESSED_LENGTH);
+    Ok(pub_bytes)
 }
 
 /// Convert an [`EcdhPublicKey`] to uncompressed SEC.1 bytes.
@@ -119,4 +201,30 @@ pub fn ec2_pubkey_to_uncompressed_sec1(key: &EcdhPublicKey) -> Result<[u8; P256_
         // Other lengths are invalid.
         _ => Err(Error::InvalidArgument),
     }
+}
+
+/// Import a raw compressed P-256 SEC.1 public key.
+pub fn import_compressed_ec2_pubkey(
+    key: &[u8; P256_X962_COMPRESSED_LENGTH],
+) -> Result<EcdhPublicKey> {
+    // Convert the key to DER format first, and import it.
+    let compressed_der = compressed_sec1_ec2_key_to_der(key)?;
+    let compressed_key = import_ec_public_key_from_spki(&compressed_der)?;
+
+    // Export the key again and re-import the key in uncompressed form, because otherwise
+    // because otherwise `ecdh` fails: https://github.com/mozilla/nss-rs/issues/136
+    let uncompressed_raw = ec2_pubkey_to_uncompressed_sec1(&compressed_key)?;
+    let uncompressed_der = sec1_ec2_key_to_der(&uncompressed_raw)?;
+    Ok(import_ec_public_key_from_spki(&uncompressed_der)?)
+}
+
+/// Clones an [`EcdhKeypair`].
+///
+/// TODO: Replace with [`Clone::clone`] when <https://github.com/mozilla/nss-rs/pull/137> is merged
+/// to `firefox-main`.
+pub fn clone_eckeypair(keypair: &EcdhKeypair) -> Result<EcdhKeypair> {
+    Ok(EcdhKeypair {
+        public: keypair.public.clone(),
+        private: keypair.private.clone(),
+    })
 }
